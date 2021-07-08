c10b38506e500bada592f784241e60e1f8701841
kelvinrandu <randukelvin@gmail.com>
2021-07-08 13:54:51 +0300

    added toggle function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('added here')
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text')
+
+    })
+
+})
